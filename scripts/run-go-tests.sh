e8b40ca7abfe8d143e1143d816f87fb9c6fbce33
Lucas Hoz <hozlucas28@gmail.com>
2026-07-10 18:27:47 -0300

    fix(scripts): change script to be compatible with macOS

--- scripts/run-go-tests.sh
+++ scripts/run-go-tests.sh
@@ -1,34 +1,37 @@
 #! /bin/bash
 
 # Parse options
-options=$(getopt -o "c:h" --long "category:,help" -- "$@")
-
-if [ $? -ne 0 ]; then
-	echo -e "\e[31mAn error occurred on parsing options.\e[0m" >&2
-	exit 1
-fi
-
-eval set -- "$options"
-
-while true; do
+while [[ $# -gt 0 ]]; do
 	case "$1" in
-		"-c" | "--category")
+		-c | --category)
 			category="$2"
 			shift 2
 			;;
-		"-h" | "--help")
-			need_help="true"
+
+		-c=* | --category=*)
+			category="${1#*=}"
+			shift 1
+			;;
+
+		-h | --help)
+			need_help='true'
 			shift 1
 			break
 			;;
-		"--")
-			shift
+
+		--)
+			shift 1
 			break
 			;;
-		*)
-			echo -e "\e[31mAn internal error occurred!\e[0m" >&2
+
+		-*)
+			printf "\e[31mAn invalid option was found!\e[0m\n" >&2
 			exit 1
 			;;
+
+        *)
+            break
+            ;;
 	esac
 done
 
@@ -37,7 +40,7 @@ if [[ -n "$need_help" ]]; then
 	printf "Usage: $0 [OPTIONS]...
 
 Options:
-	-t, --category     category to test (e.g., software-engineer-prepare-kit)
+	-t, --category     category to test (e.g., 'software-engineer-prepare-kit')
 	-h, --help         display this help and exit
 
 If \`--category\` is not provided, all tests will be run.
@@ -60,7 +63,7 @@ test_challenges() {
 cd $(cd "$(dirname "$0")/.." && pwd)
 
 if [[ $? -ne 0 ]]; then
-	echo -e "\e[31mFailed to change directory to project root.\e[0m" >&2
+	printf "\e[31mFailed to change directory to project root.\e[0m\n" >&2
 	exit 1
 fi
 
@@ -68,8 +71,8 @@ fi
 if [[ -z "$category" ]]; then
 	# Run all tests if category is not specified
 	categories=(
-		"Security"
-		"Software Engineer Prepare Kit"
+		'Security'
+		'Software Engineer Prepare Kit'
 	)
 
 	for category in "${categories[@]}"; do
@@ -78,14 +81,16 @@ if [[ -z "$category" ]]; then
 else
 	# Run tests for a specific category
 	case "$category" in
-		"security")
-			category="Security"
+		security)
+			category='Security'
 			;;
-		"software-engineer-prepare-kit")
-			category="Software Engineer Prepare Kit"
+
+		software-engineer-prepare-kit)
+			category='Software Engineer Prepare Kit'
 			;;
+
 		*)
-			echo -e "\e[31mInvalid category!\e[0m" >&2
+			printf "\e[31mInvalid category!\e[0m\n" >&2
 			exit 1
 			;;
 	esac
